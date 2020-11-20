b82cdd0a937f86ee731a122947d67e7945e05d35
Richard Neil Ilagan (Carding) <705310+richardneililagan@users.noreply.github.com>
2020-11-20 23:56:06 +0800

    fix: switches ESC for grave escape

--- layers/keymaps/colemak-dhm.h
+++ layers/keymaps/colemak-dhm.h
@@ -23,7 +23,7 @@
 #define KC_MOD   MO(_MOD)
 
 #define LAYER_COLEMAK_DHM LAYOUT( \
-  KC_GRV,   KC_7,    KC_5,    KC_3,    KC_1,    KC_9,                       KC_8,    KC_0,    KC_2,    KC_4,    KC_6,    KC_BSPC, \
+  KC_GESC,  KC_7,    KC_5,    KC_3,    KC_1,    KC_9,                       KC_8,    KC_0,    KC_2,    KC_4,    KC_6,    KC_BSPC, \
   KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                       KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_QUOT, \
   KC_LCTRL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                       KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,  \
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,     KC_MUTE, KC_MOD,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
