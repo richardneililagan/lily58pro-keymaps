624c616065a18b7bd57e81d468c90e10e0c7b900
Richard Neil Ilagan (Carding) <705310+richardneililagan@users.noreply.github.com>
2020-11-21 12:31:29 +0800

    feat: adds custom layer names to read_layer_state

---
+++ layers/layer_state_reader.c
@@ -0,0 +1,56 @@
+/**
+ *  Defines layer names for the custom layers used by the keyboard.
+ */
+
+#include QMK_KEYBOARD_H
+#include <stdio.h>
+#include "lily58.h"
+
+// :: make sure these correspond with the layers defined
+//    in `definitions.h`
+#define L_BASE 0
+#define L_GAMER (1 << 1)
+#define L_LOWER (1 << 2)
+#define L_RAISE (1 << 3)
+#define L_ADJUST (1 << 4)
+#define L_ADJUST_TRI (L_RAISE | L_LOWER)  // :: when RAISE and LOWER are held together
+#define L_MOD (1 << 5)
+
+// :: ---
+
+char layer_state_str[24];
+
+const char *read_layer_state(void) {
+  switch (layer_state) {
+    case L_BASE: 
+      snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Colemak-DHm");
+      break;
+
+    case L_GAMER:
+      snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Gamer");
+      break;
+
+    case L_LOWER:
+      snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
+      break;
+
+    case L_RAISE:
+      snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
+      break;
+
+    case L_ADJUST:
+    case L_ADJUST_TRI:
+      snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
+      break;
+
+    case L_MOD:
+      snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Mod");
+      break;
+
+    // :: ---
+    default:
+      snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
+  }
+
+  return layer_state_str;
+}
