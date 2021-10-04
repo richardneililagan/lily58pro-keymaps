f4b9b190608f3b0be55d1912c4c62853e6431f35
Richard Neil Ilagan <705310+richardneililagan@users.noreply.github.com>
2021-10-04 12:38:20 +0800

    feat: ctrl -> mod tap ctrl / esc

--- rules.mk
+++ rules.mk
@@ -17,6 +17,7 @@ AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
+BOOTLOADER = qmk-dfu
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
